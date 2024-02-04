eb2bf11e0e3a1f35e4cbf5f6a697b44f2f63e4e6
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-02-04 22:42:35 +0545

    a

---
+++ Competitive Programming/LeetCode/Palindrome Number.py
@@ -0,0 +1,45 @@
+'''
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+Follow up: Could you solve it without converting the integer to a string?
+'''
+
+
+def isPalindrome(x):
+    t = 0
+    c = x
+    while x > 0:
+        t = t*10 + x % 10
+        x = x/10
+        
+    if t == c:
+        return True
+    
+    return False
+
+print(isPalindrome(121))
