4156f0879c91d1cbd5587db496358260c7f46b0d
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-07-09 23:30:29 +0545

    added a new file

--- Competitive Programming/HackerEarth/TramRide.cpp
+++ Competitive Programming/HackerEarth/TramRide.cpp
@@ -5,11 +5,9 @@ long long solve (int N, int start, int finish, vector<int> Ticket_cost)  {
     if(start==finish)
         return 0;  
 
-
     int i;
     long long forward_cost = 0;
     for (i = start-1; i != finish-1; i = (i + 1) % N) {
-
         forward_cost += Ticket_cost[i];
     }
 
@@ -28,8 +26,7 @@ int main() {
     int finish;
     cin >> finish;
     vector<int> Ticket_cost(N);
-    for(int i_Ticket_cost = 0; i_Ticket_cost < N; i_Ticket_cost++)
-    {
+    for(int i_Ticket_cost = 0; i_Ticket_cost < N; i_Ticket_cost++){
         cin >> Ticket_cost[i_Ticket_cost];
     }
 
